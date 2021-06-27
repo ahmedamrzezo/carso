5ab20531a762506ee15020eac772eb88d9cf65c8
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-06-27 23:37:24 +0200

    expense props

--- src/components/expenses/expense-item/ExpenseItem.js
+++ src/components/expenses/expense-item/ExpenseItem.js
@@ -1,11 +1,18 @@
 import './ExpenseItem.scss';
 
 function ExpenseItem() {
+	const expense = {
+		name: 'My exp',
+		description: 'Desc',
+		date: new Date().toDateString()
+	};
+	// const expenseName = 'My exp';
 	return (
 		<div className="expense-item">
 			<div>
-				<h4>Title</h4>
-				<p>Desctripton</p>
+				<h4>{expense.name}</h4>
+				<p>{expense.description}</p>
+				<small>{expense.date}</small>
 			</div>
 		</div>
 	);
