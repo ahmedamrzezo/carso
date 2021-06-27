d50667554fdc875ddd6e35d559426169c80b2a77
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-06-27 23:28:53 +0200

    expense item compo

---
+++ src/components/expenses/expense-item/ExpenseItem.js
@@ -0,0 +1,14 @@
+import './ExpenseItem.scss';
+
+function ExpenseItem() {
+	return (
+		<div className="expense-item">
+			<div>
+				<h4>Title</h4>
+				<p>Desctripton</p>
+			</div>
+		</div>
+	);
+}
+
+export default ExpenseItem;
