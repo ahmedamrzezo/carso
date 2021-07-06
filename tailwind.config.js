4c98ad4dcfa2b4f38a9b41ac18ec75bd61572def
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-07-07 00:01:25 +0200

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      primary_color: '#96616b',
+      primary_color_50: 'rgb(150 97 107 / 50%)',
+      primary_light_color: '#ffead0',
+      primary_dark_color: '#113537',
+      white: '#fff',
+      gray: colors.trueGray,
+    }
   },
   variants: {
     extend: {},
