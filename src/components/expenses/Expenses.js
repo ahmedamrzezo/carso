ec52d55a4f938db7ebec7ca9748fa2d10cc14b78
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-07-08 19:21:40 +0200

    handle insert expense to db

--- src/components/expenses/Expenses.js
+++ src/components/expenses/Expenses.js
@@ -1,5 +1,7 @@
 import ExpenseItem from "./expense-item/ExpenseItem";
 import { useEffect, useState } from 'react';
+import { AddExpense } from "./AddExpense/AddExpense";
+import { environment } from "../../environment/environment";
 
 function Expenses() {
 
@@ -9,17 +11,56 @@ function Expenses() {
 
 	const getExpenses = async () => {
 		try {
-			const res = await fetch('https://expensat-api.herokuapp.com/expenses');
+			const res = await fetch(`${environment.apiUrl}/expenses`);
 			setExpenses(await res.json());
 		} catch (error) {
 			console.error(error);
 		}
 	};
 
+	const addNewExpense = () => {
+		const newExpense = {
+			date: new Date().toISOString(),
+			amount: '',
+			title: '',
+			description: '',
+			id: new Date().getTime(),
+			editable: true
+		};
+		setExpenses([...expenses, newExpense]);
+	};
+
+	const insertExpense = async (expense, id) => {
+		try {
+			const res = await fetch(`${environment.apiUrl}/expenses`, {
+				method: 'POST',
+				headers: {
+					'Accept': 'application/json',
+					'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(expense)
+			});
+			const data = await res.json();
+			if (!data.errors) {
+				console.log(id);
+				return setExpenses([...expenses.filter((ex) => ex.id !== id), data]);
+			}
+			throw new Error(data.message);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+
 	return (
-		<div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 gap-10">
-			{expenses.map((expense, id) => <ExpenseItem expense={expense} key={id} />)}
-			<ExpenseItem key="new" empty={true} />
+		<div className="grid lg:grid-cols-2 md:grid-cols-2 grid-cols-1 auto-rows-fr gap-10">
+			{expenses.map((expense) => {
+				if (expense.editable) {
+					return <AddExpense key={expense.id} onExpenseAdd={insertExpense} id={expense.id} />;
+				}
+				return <ExpenseItem expense={expense} key={expense.id} editable={expense.editable} />;
+			})}
+			<ExpenseItem key="empty" empty={true} onAddNewExpenseComp={addNewExpense} />
 		</div>);
 }
 
