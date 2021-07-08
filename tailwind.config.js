7502625157f5023fdeb206dcc298be7975806fdc
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-07-08 18:54:54 +0200

    use tailwindcss jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  mode: 'jit',
+  purge: ['./src/**/*.{js}', './public/index.html'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    maxWidth: {
+      '65': '65%'
+    },
     extend: {},
     colors: {
       transparent: 'transparent',
