06336494a6e00eeafda3d167ccc28f211d8e4523
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-07-08 22:57:16 +0200

    expand button

--- src/components/shared/ui/button/Button.js
+++ src/components/shared/ui/button/Button.js
@@ -1,12 +1,17 @@
 import './Button.scss';
 
 const Button = ({ btnType, children, clickHandler, type }) => {
+	const shadow = 'shadow hover:shadow-xl';
+	const size = 'w-48 h-20 ';
+	const hover = 'transform hover:-translate-y-2 transition-all';
+
 	const types = {
-		primary: 'bg-primary_color text-white',
-		bordered: 'border border-primary_color text-primary_color'
+		primary: `rounded-xl bg-primary_color text-white ${shadow} ${size} ${hover} `,
+		bordered: `rounded-xl border border-primary_color text-primary_color ${shadow} ${size} ${hover} `,
+		text: `p-4 text-primary_color`
 	};
 
-	let classes = `rounded-xl font-semibold text-2xl w-48 h-20 flex items-center justify-center capitalize cursor-pointer shadow hover:shadow-xl transform hover:-translate-y-2 transition-shadow transition-transform delay-75 ${types[btnType]}`;
+	let classes = `font-semibold text-2xl flex items-center justify-center capitalize cursor-pointer m delay-75 ${types[btnType]}`;
 
 	return (
 		<button className={classes} onClick={clickHandler} type={type}>
