f64c447bb5ca5ba8fef5774e470f6a92ec69f4eb
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-07-10 23:12:12 +0200

    user children

--- src/components/expenses/expense-date/ExpenseDate.js
+++ src/components/expenses/expense-date/ExpenseDate.js
@@ -1,16 +1,19 @@
 import './ExpenseDate.scss';
 
-function ExpenseDate({ date }) {
+function ExpenseDate({ children, date }) {
 	const day = new Date(date).toLocaleString('en-GB', { day: '2-digit' });
 	const month = new Date(date).toLocaleString('en-GB', { month: 'short' });
 	const year = new Date(date).getFullYear();
 
 	return (
 		<div className="bg-primary_light_color flex flex-wrap items-center justify-center p-4 w-48 h-48 rounded-xl">
-			<span className="font-bold w-full text-center text-8xl">
-				{day}
-			</span>
-			<span className="text-4xl text-gray-500">{month} {year}</span>
+			{date &&
+				<div>
+					<span className="font-bold w-full text-center text-8xl">{day}</span>
+					<br />
+					<span className="text-4xl text-gray-500">{month} {year}</span>
+				</div>}
+			{!date && children}
 		</div>
 	);
 }
