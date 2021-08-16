2c00eb4988c30ea0e8b48c946d01e6cf9d234596
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-08-16 18:02:50 +0200

    button ui adjustment

--- src/components/shared/ui/button/Button.js
+++ src/components/shared/ui/button/Button.js
@@ -1,17 +1,29 @@
 import './Button.scss';
 
-const Button = ({ className, btnType, children, clickHandler, type }) => {
+const Button = ({
+	className,
+	btnType,
+	children,
+	clickHandler,
+	type,
+	haveHover = true,
+	size = 'default'
+}) => {
 	const shadow = 'shadow hover:shadow-xl';
-	const size = 'w-48 h-20 ';
-	const hover = 'transform hover:-translate-y-2 transition-all';
+	const sizes = {
+		default: 'w-48 h-20',
+		small: 'w-32 h-16',
+		dynamic: 'px-8 py-3'
+	};
+	const hoverClasses = 'transform hover:-translate-y-2 transition-all' + shadow;
 
 	const types = {
-		primary: `rounded-xl bg-primary_color text-white ${shadow} ${size} ${hover} `,
-		bordered: `rounded-xl border border-primary_color text-primary_color ${shadow} ${size} ${hover} `,
+		primary: `rounded-xl bg-primary_color text-white ${sizes[size]}`,
+		bordered: `rounded-xl border border-primary_color text-primary_color ${sizes[size]} `,
 		text: `p-4 text-primary_color`
 	};
 
-	let classes = `font-semibold text-2xl flex items-center justify-center capitalize cursor-pointer m delay-75 ${types[btnType]} ${className}`;
+	let classes = `font-semibold text-2xl flex items-center justify-center capitalize cursor-pointer m delay-75 ${types[btnType]} ${haveHover && hoverClasses} ${className}`;
 
 	return (
 		<button className={classes} onClick={clickHandler} type={type}>
