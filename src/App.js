e50750e359b53030fde8baebf01aac5160cd0fa6
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-08-16 18:02:38 +0200

    auth context provider and logged state

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import './App.scss';
 import Header from './layout/header/Header';
 import Footer from './layout/footer/Footer';
 import Expenses from './components/expenses/Expenses';
+import Login from './components/auth/login/login';
+import AuthContext from './store/auth-context';
+import { useContext } from 'react';
 
 function App() {
+  const authCtx = useContext(AuthContext);
 
   return (
     <div className="App">
       <Header />
 
-      <main>
-        <div className="container mx-auto">
+      {
+        !authCtx.isLogged &&
+        <Login />
+      }
+      <main className="container mx-auto">
 
-          <Expenses />
+        <Expenses />
 
-        </div>
       </main>
 
       <Footer />
