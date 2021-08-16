c82b1e18156cc76445dfd1a03b6323d904d30a01
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-08-16 18:03:11 +0200

    login component

--- src/components/auth/login/login.jsx
+++ src/components/auth/login/login.jsx
@@ -1,10 +1,36 @@
+import { useContext, useReducer } from 'react';
 import { environment } from '../../../environment/environment';
+import AuthContext from '../../../store/auth-context';
 import Button from '../../shared/ui/button/Button';
-import FormControl from '../../shared/ui/form-control/FormControl';
+import FormField from '../../shared/ui/form-field/FormField';
 import { getFormValues } from '../../shared/utils/Form';
 import { getOptions } from '../../shared/utils/Http';
 
+const reducer = (state, action) => {
+	switch (action.type) {
+		case 'USER_EMAIL':
+			return {
+				emailVal: action.emailVal,
+				passwordVal: state.passwordVal,
+			};
+		case 'USER_PASSWORD':
+			return {
+				passwordVal: action.passwordVal,
+				emailVal: state.emailVal,
+			};
+		default:
+			console.log('default', action);
+			return {
+				value: '',
+				emailValid: false,
+				passwordValid: false,
+			};
+	}
+};
+
 const Login = () => {
+	const authCtx = useContext(AuthContext);
+
 	const login = async (ev) => {
 		ev.preventDefault();
 		const values = getFormValues(ev.target.elements, ['email', 'password']);
@@ -16,7 +42,31 @@ const Login = () => {
 
 		const userData = await res.json();
 
+		authCtx.loginHandler();
+
 		localStorage.setItem('user', JSON.stringify(userData));
+	};
+
+	const [formState, dispatchEmail] = useReducer(reducer, {
+		emailValid: undefined,
+		passwordValid: undefined,
+	});
+
+	const emailChange = (val) => {
+		// console.log(d);
+		// dispatchEmail({ type: 'USER_EMAIL', val: val || {} });
+	};
+
+	const emailBlur = (val) => {
+		dispatchEmail({ type: 'USER_EMAIL', emailVal: val });
+	};
+
+	const passwordBlur = (val) => {
+		dispatchEmail({ type: 'USER_PASSWORD', passwordVal: val });
+	};
+
+	const passwordChange = (val) => {
+		console.log(val);
 	};
 
 	return (
@@ -26,31 +76,47 @@ const Login = () => {
 			<form onSubmit={login}>
 				{/* email */}
 
-				<div className="form-group w-100 my-4">
-					<FormControl
-						controlType={'input'}
-						type="email"
-						placeholder="Enter your email"
-						className="form-control"
-						name="email"
-						autoComplete="email"
-						required
-					/>
-				</div>
+				<FormField
+					fieldType={'input'}
+					type="email"
+					placeholder="Enter your email"
+					className="form-control"
+					name="email"
+					autoComplete="email"
+					required
+					fieldChange={emailChange}
+					fieldBlur={emailBlur}
+					debounce={500}
+					isValid={
+						formState.emailVal === undefined
+							? undefined
+							: formState.emailVal.length > 5
+							? true
+							: false
+					}
+				/>
 
 				{/* password */}
 
-				<div className="form-group">
-					<FormControl
-						controlType={'input'}
-						type="password"
-						placeholder="Enter your password"
-						className="form-control"
-						name="password"
-						autoComplete="current-password"
-						required
-					/>
-				</div>
+				<FormField
+					fieldType={'input'}
+					type="password"
+					placeholder="Enter your password"
+					className="form-control"
+					name="password"
+					autoComplete="current-password"
+					fieldChange={passwordChange}
+					fieldBlur={passwordBlur}
+					debounce={500}
+					required
+					isValid={
+						formState.passwordVal === undefined
+							? undefined
+							: formState.passwordVal.length > 4
+							? true
+							: false
+					}
+				/>
 
 				<Button className="mt-4" btnType="primary" type="submit">
 					Let's go
