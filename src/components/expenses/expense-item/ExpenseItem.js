d3025dfa9258d62240bd3d44b46407c7bf221500
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-09-05 22:59:25 +0200

    margin

--- src/components/expenses/expense-item/ExpenseItem.js
+++ src/components/expenses/expense-item/ExpenseItem.js
@@ -7,7 +7,7 @@ export default function ExpenseItem({ expense, empty, onAddNewExpenseComp }) {
 	if (empty) {
 		return (
 			<div className="rounded-xl flex justify-center items-center border-4 border-dashed border-primary_color cursor-pointer" onClick={onAddNewExpenseComp}>
-				<span className="text-8xl border-4 border-dashed border-primary_color w-40 h-40 rounded-full flex justify-center items-center text-primary_color"><PlusIcon className="w-16 text-primary_color"></PlusIcon></span>
+				<span className="text-8xl border-4 border-dashed border-primary_color w-40 h-40 rounded-full flex justify-center items-center text-primary_color my-20"><PlusIcon className="w-16 text-primary_color"></PlusIcon></span>
 			</div>
 		);
 	}
