76eab1887a2187a735586d14704decef9c437b43
ahmedamrzezo <ahmedamrzezo@gmail.com>
2021-09-18 19:37:52 +0200

    navigate on login or logout

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
 
 const AuthContext = React.createContext({
 	isLogged: false,
@@ -16,14 +17,22 @@ export const AuthContextProvider = ({ children }) => {
 		}
 	}, []);
 
-	const loginHandler = () => {
+	const navigate = useHistory();
+
+	const loginHandler = (data) => {
 		setIsLogged(true);
+
+		localStorage.setItem('user', JSON.stringify(data));
+
+		navigate.push('/expenses');
 
 	};
 
 	const logoutHandler = () => {
 		localStorage.removeItem('user');
 		setIsLogged(false);
+
+		navigate.push('/login');
 	};
 
 	return <AuthContext.Provider value={{ isLogged, loginHandler, logoutHandler }}>{children}</AuthContext.Provider>;
